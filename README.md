95fda2b9ecd933597f8cc90d06efc577f3ed7d14
Jeongjun Lee <jun@Jeongjuns-MacBook-Air.local>
2021-12-09 21:48:18 +0900

    repo init

---
+++ README.md
@@ -0,0 +1,5 @@
+# HELLO
+This is modify torchvision`s model output_features 
+ 
+1212879@naver.com
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name                = 'torchedit',
+    version             = '0.1',
+    description         = 'for modify torchvision model out_features',
+    author              = 'LEEJEONGJUN',
+    author_email        = '1212879@naver.com',
+    # 배포하는 패키지의 url을 적어줍니다. 보통 github 링크를 적습니다.
+    url                 = 'https://github.com/doorBW/pypi_deploy_test',
+    # 배포하는 패키지의 다운로드 url을 적어줍니다.
+    download_url        = 'https://github.com/doorBW/pypi_deploy_test/archive/master.zip',
+    install_requires    =  ['torch','torchvision'],
+    # 등록하고자 하는 패키지를 적는 곳입니다.
+    # 우리는 find_packages 라이브러리를 이용하기 때문에 아래와 같이 적어줍니다.
+    # 만약 제외하고자 하는 파일이 있다면 exclude에 적어줍니다.
+    packages            = find_packages(exclude = []),
+    # 패키지의 키워드를 적습니다.
+    keywords            = ['pypi deploy'],
+    # 해당 패키지를 사용하기 위해 필요한 파이썬 버전을 적습니다.
+    python_requires     = '>=3',
+    # 파이썬 파일이 아닌 다른 파일을 포함시키고 싶다면 package_data에 포함시켜야 합니다.
+    package_data        = {},
+    # 위의 package_data에 대한 설정을 하였다면 zip_safe설정도 해주어야 합니다.
+    zip_safe            = False,
+    # PyPI에 등록될 메타 데이터를 설정합니다.
+    # 이는 단순히 PyPI에 등록되는 메타 데이터일 뿐이고, 실제 빌드에는 영향을 주지 않습니다.
+    classifiers         = [
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
